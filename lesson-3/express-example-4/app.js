053eac6e980261a319c50462d533658fee62432b
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2024-04-29 22:21:15 +0300

    finish lesson 3

---
+++ lesson-3/express-example-4/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+import moviesRouter from "./routes/moviesRouter.js";
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api/movies", moviesRouter);
+
+app.use((req, res)=> {
+    res.status(404).json({
+        message: "Not Found"
+    })
+})
+
+app.listen(3000, (error)=> {
+    if(error) {
+        return console.log(`Error ${error.message}`);
+    }
+    console.log("Server running on 3000 PORT");
+})
